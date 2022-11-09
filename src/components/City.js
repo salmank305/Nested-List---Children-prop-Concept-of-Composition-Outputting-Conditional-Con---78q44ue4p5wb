9daf168dc5bd424a6dd9863796273fb8ec085383
Salman khatik <110889430+salmank305@users.noreply.github.com>
2022-11-09 20:26:35 +0530

    Update City.js

--- src/components/City.js
+++ src/components/City.js
@@ -1,1 +1,10 @@
 
+import React from 'react'
+
+const City = ({displayTowns, id, cityName}) => {
+  return (
+    <div id={`city${id+1}`} onClick={() => displayTowns(id)}>{cityName}</div>
+  )
+}
+
+export default City;
